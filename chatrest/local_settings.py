531746c7acefdbec4febfd4936791f3ee32fb39d
kolamor <kolamorev@mail.ru>
2018-10-31 02:43:51 +0300

    first commit

---
+++ chatrest/local_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangorest_db',
+        'USER': 'kola',
+        'PASSWORD': 'romromrom1',
+
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
